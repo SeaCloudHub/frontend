bc419f131fed025904617c7b110e1111de0e2ab4
=Minnas <=phannhattrieu01234@gmail.com>
2024-03-02 18:26:09 +0700

    fix: navbar

--- src/components/core/button/ButtonContainer.tsx
+++ src/components/core/button/ButtonContainer.tsx
@@ -1,9 +1,10 @@
 import { Button, styled } from '@mui/material';
 
 type ButtonContainerProps = {
-  title: string;
+  title?: string;
   onClick?: () => void;
   color?: string;
+  size?: number | string;
   background?: string;
   backgroundHover?: string;
   icon?: React.ReactNode;
@@ -14,15 +15,20 @@ const ButtonContainerStyle = styled(Button)(
     background,
     borderRadius,
     Color,
+    Size,
     backGroundHover,
   }: {
-    backGroundHover?: string;
     background?: string;
     borderRadius?: string;
     Color?: string;
+    Size?: number | string;
+    backGroundHover?: string;
   }) => ({
     fontWeight: 'bold',
     color: Color,
+    width: Size,
+    height: Size,
+    justifyContent: 'flex-end',
     backgroundColor: background,
     borderRadius: borderRadius,
     textTransform: 'none',
@@ -32,10 +38,11 @@ const ButtonContainerStyle = styled(Button)(
   }),
 );
 
-const ButtonContainer = ({ title, color, background, backgroundHover, icon }: ButtonContainerProps) => {
+const ButtonContainer = ({ title, color, background, backgroundHover, icon, size }: ButtonContainerProps) => {
   return (
     <ButtonContainerStyle
       startIcon={icon}
+      Size={size}
       backGroundHover={backgroundHover}
       variant='contained'
       Color={color}
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,11 +1,45 @@
+import IconifyIcon from '../core/Icon/IConCore';
+import ButtonContainer from '../core/button/ButtonContainer';
+import ButtonIcon from '../core/button/ButtonIcon';
+
 type NavbarProps = {
   isShrink: boolean;
   phoneMode: boolean;
 };
 const Navbar = ({ isShrink, phoneMode }: NavbarProps) => {
-  return <div className={`${phoneMode ? 'ml-0 !important' : ''} nav-bar ${isShrink ? 'nav-bar-shrink' : ''}`}>
-    
-  </div>;
+  return (
+    <div className={`${phoneMode ? 'pl-0 !important' : ''} nav-bar ${isShrink ? 'nav-bar-shrink' : ''}`}>
+      <div className='px-3 flex items-center justify-between w-full h-full'>
+        <div className={`${phoneMode ? 'w-full' : 'w-6/12'} flex h-[50px] items-center justify-center`}>
+          <div className={`${phoneMode ? 'block mr-2' : 'hidden'}`}>
+            <ButtonIcon icon='ic:outline-menu' size={25} />
+          </div>
+          <input className={`${phoneMode ? 'h-3/4' : ' h-full'} w-full `} placeholder='Search file' />
+          <ButtonContainer
+            size={phoneMode ? 35 : 50}
+            background='#063768'
+            color='white'
+            icon={<IconifyIcon icon={'ion:search-sharp'} />}
+          />
+        </div>
+        <div className={`${phoneMode ? 'hidden' : 'flex items-center w-3/12 '}`}>
+          <div className='rounded-full h-full '>
+            <img
+              src='https://student.hcmus.edu.vn/_next/image?url=%2Fhcmus-logo.png&w=384&q=75'
+              className='max-h-[50px] object-contain'
+            />
+          </div>
+          <div className='flex flex-col items-start  justify-center'>
+            <h6 className='statement-bold'>Phan Nhật Triều</h6>
+            <div className='flex items-center justify-center'>
+              <p className='statement-upper-medium'>phannhattrieu012@gmail.com</p>
+              <ButtonIcon icon='teenyicons:down-solid' size={15} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
