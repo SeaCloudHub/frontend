7cf26deab88fdd88fb7aba72a135eeeb23500ad9
sulaidat <tuyenvx.lqd@gmail.com>
2024-03-03 14:40:07 +0700

    feat: file card

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import { Box, CssBaseline, CssVarsProvider } from '@mui/joy';
+import renderFilters from './components/pages/DashboardPage';
+import FileCard from './components/core/file-card/FileCard';
+
 function App() {
   return (
     <>
-      <div>Mode {import.meta.env.VITE_BACKEND_API_ENDPOINT}</div>
+      <CssVarsProvider disableTransitionOnChange>
+        <CssBaseline />
+        <div className='flex flex-wrap gap-2'>
+          <div className='w-72'>
+            <FileCard title='flag.doc' size='1KB' />
+          </div>
+          <div className='w-72'>
+            <FileCard title='flag.mp3' size='1KB' />
+          </div>
+          <div className='w-72'>
+            <FileCard title='flag.png' size='1KB' />
+          </div>
+          <div className='w-72'>
+            <FileCard title='flag.txt' size='1KB' />
+          </div>
+          <div className='w-72'>
+            <FileCard
+              title='flag.txt'
+              size='1KB'
+              preview='https://i.pinimg.com/280x280_RS/01/6a/45/016a45c595efdc6d97c7fbc5a562f78b.jpg'
+            />
+          </div>
+        </div>
+        <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2 }}>{renderFilters()}</Box>
+        <div className='flex flex-row gap-x-2 '>{renderFilters()}</div>
+        <div className='w-40'>{renderFilters()}</div>
+      </CssVarsProvider>
     </>
   );
 }
---
+++ src/components/core/file-card/FileCard.tsx
@@ -0,0 +1,86 @@
+import { EllipsisVerticalIcon, PencilIcon, ShareIcon, TrashIcon } from '@heroicons/react/16/solid';
+import { DocumentIcon, MusicalNoteIcon, PhotoIcon } from '@heroicons/react/24/outline';
+import { DocumentTextIcon } from '@heroicons/react/24/outline';
+import { AspectRatio, Box, Card, CardOverflow, Dropdown, IconButton, Menu, MenuButton, MenuItem, Typography } from '@mui/joy';
+
+interface FileCardProps {
+  title: string;
+  size: string;
+  preview?: string;
+}
+
+const menuItems = [
+  { icon: <PencilIcon />, label: 'Rename file' },
+  { icon: <ShareIcon />, label: 'Share file' },
+  { icon: <TrashIcon />, label: 'Delete file' },
+];
+
+const supportedFileTypes = [
+  { type: 'doc', icon: <DocumentTextIcon /> },
+  { type: 'docx', icon: <DocumentTextIcon /> },
+  { type: 'mp3', icon: <MusicalNoteIcon /> },
+  { type: 'png', icon: <PhotoIcon /> },
+  { type: 'jpg', icon: <PhotoIcon /> },
+];
+
+const type2Icon = (type: string) => {
+  const icon = supportedFileTypes.find((fileType) => fileType.type === type);
+  return icon ? icon.icon : <DocumentIcon />;
+};
+
+const FileCard: React.FC<FileCardProps> = ({ title, size, preview }) => {
+  const type = title.split('.').length > 1 ? title.split('.').pop() : 'unknown';
+  return (
+    <div className='shadow-sm'>
+      <Card variant='outlined' size='sm'>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Box sx={{ flex: 1 }}>
+            <Typography level='title-md'>{title}</Typography>
+            <Typography level='body-sm'>{size}</Typography>
+          </Box>
+
+          <Dropdown>
+            <MenuButton
+              variant='plain'
+              size='sm'
+              sx={{
+                padding: 0,
+              }}>
+              <IconButton component='span' variant='plain' color='neutral' size='sm'>
+                <EllipsisVerticalIcon className='w-6 h-6' />
+              </IconButton>
+            </MenuButton>
+
+            <Menu placement='bottom-start' size='sm'>
+              {menuItems.map((item, index) => (
+                <MenuItem key={index}>
+                  <div
+                    className={`flex 
+                  ${item.label.includes('Delete') ? 'text-red-500' : ''}
+                  `}>
+                    <div className='w-6 h-6 mr-2'>{item.icon}</div>
+                    <div className='text-nowrap mr-2'>{item.label}</div>
+                  </div>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Dropdown>
+        </Box>
+
+        <CardOverflow>
+          <AspectRatio color='primary'>
+            {preview ? (
+              <img src={preview} />
+            ) : (
+              <div>
+                <div className='w-12 h-12'>{type2Icon(type as string)}</div>
+              </div>
+            )}
+          </AspectRatio>
+        </CardOverflow>
+      </Card>
+    </div>
+  );
+};
+
+export default FileCard;
