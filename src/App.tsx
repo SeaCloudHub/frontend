1f04253e9499558b3a14dee5ba2f3bbb460e6733
20120474 <84512387+kimhieu153255@users.noreply.github.com>
2024-03-03 12:32:52 +0700

    feat: auth UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import Register from './pages/auth/Signup';
-import Login from './pages/auth/Login';
-
 function App() {
   return (
     <>
-      {/* <div>Mode {import.meta.env.VITE_BACKEND_API_ENDPOINT}</div> */}
-      <Login />
-      {/* <Register /> */}
+      <div>Mode {import.meta.env.VITE_BACKEND_API_ENDPOINT}</div>
     </>
   );
 }
