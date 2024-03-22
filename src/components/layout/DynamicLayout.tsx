5ca17dec460a4210c99a18e2bbdea842b6eb762b
Minnas <phannhattrieu012@gmail.com>
2024-03-22 14:58:02 +0700

    fix: set side-bar regard to role

--- src/components/layout/DynamicLayout.tsx
+++ src/components/layout/DynamicLayout.tsx
@@ -1,27 +1,17 @@
-import { PropsWithChildren, useEffect, useState } from 'react';
+import { PropsWithChildren } from 'react';
+import { useSession } from '../../store/auth/session';
 import { useScreenMode } from '../../store/responsive/screenMode';
-import { Role } from '../../utils/enums/role.enum';
 import { ScreenMode } from '../../utils/enums/screen-mode.enum';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 
 const DynamicLayout = ({ children }: PropsWithChildren) => {
-  const { shrinkMode, screenMode, updateScreenMode, updateShrinkMode } = useScreenMode();
-  const [role, setRole] = useState<Role>(Role.ADMIN);
-  const pathName: string = 'User';
- 
-  useEffect(() => {
-    if (pathName.startsWith('Admin')) {
-      setRole(Role.ADMIN);
-    }
-    if (pathName.startsWith('User')) {
-      setRole(Role.USER);
-    }
-  }, [pathName]);
+  const { shrinkMode, screenMode } = useScreenMode();
+  const role = useSession((state) => state.role);
   return (
     <>
       <Navbar phoneMode={screenMode == ScreenMode.MOBILE} isShrink={shrinkMode} />
-      {!(screenMode == ScreenMode.MOBILE) && <Sidebar shrinkMode={shrinkMode} role={role} />}
+      {!(screenMode == ScreenMode.MOBILE) && <Sidebar shrinkMode={shrinkMode} role={role!} />}
       <div
         className={`content-default-mode p-3 ${shrinkMode ? 'content-shrink-mode' : ''}
         ${screenMode == ScreenMode.MOBILE ? 'ml-0' : ''}`}>
